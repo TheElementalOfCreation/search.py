2c6b42c69f86ecd141d668a92c6b41f5464b8bdf
Ken Peterson <arceusthe@gmail.com>
2018-07-20 20:33:08 -0700

    Add files via upload

--- update.py
+++ update.py
@@ -1,19 +1,22 @@
 import httplib;
 import os;
+import subprocess;
+
+def get(conn, x):
+	conn.request('GET', url.format(x));
+	data = conn.getresponse().read();
+	file1 = open(x, 'wb');
+	file1.write(data);
+	file1.close();
+
+__git__ = '/'.join(__file__.replace('\\', '/').split('/')[:-1] + ['git', 'bin']);
 
 files = [
 	'msg.py',
 	'toPdf.bat',
 	'search.py',
 	'SEARCH.bat',
-	'update.py',
-	'UPDATE.bat',
 	'Version',
-	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/path.py',
-	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/__init__.py',
-	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/__init__.py',
-	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/progress_bar.py',
-	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/types.py',
 ];
 
 url = '/TheElementalOfCreation/search.py/master/{}';
@@ -21,12 +24,28 @@ data = {};
 version = None;
 
 try:
-	
+	a = open('Version', 'r');
+	version = a.split('.');
+	a.close();
 except:
-	
+	pass;
 
 a = httplib.HTTPSConnection('raw.githubusercontent.com');
-for x in files:
-	a.request('GET', url.format(x));
-	data[x] = a.getresponse().read();
-	
+a.request('GET', url.format('Version'));
+v = a.getresponse().read().split('.');
+if v[0] != version[0]:
+	# update.py and/or UPDATE.bat update
+	f = file.open('Version');
+	f.write('.'.join((v[0], '-update-')));
+	get(a, 'update.py');
+	get(a, 'UPDATE.bat');
+	exit(102);
+
+elif v[1] != version[1]:
+	# Update of other file(s)
+	for x in files:
+		get(a, x);
+
+os.envrion['PATH'] += __git__ + ';';
+os.envrion.update();
+subprocess.call(['pypy2-v5.9.0-win32\\pypy.exe', '-m', 'pip', 'install', 'git+https://github.com/TheElementalOfCreation/creatorUtils']);
