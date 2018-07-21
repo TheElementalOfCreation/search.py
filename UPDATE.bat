22af7cdc9d4a4e93681a8fb5428660d19f2ae49e
Ken Peterson <arceusthe@gmail.com>
2018-07-20 20:04:30 -0700

    Add files via upload

--- UPDATE.bat
+++ UPDATE.bat
@@ -1,3 +1,6 @@
 @echo off
 pypy2-v5.9.0-win32\pypy.exe update.py
+if %ERRORLEVEL% EQU 102 (
+	UPDATE.bat
+)
 pause
--- update.py
+++ update.py
@@ -1,6 +1,5 @@
 import httplib;
 import os;
-import _md5;
 
 files = [
 	'msg.py',
@@ -8,37 +7,26 @@ files = [
 	'search.py',
 	'SEARCH.bat',
 	'update.py',
-	'UPDATE.bat'
+	'UPDATE.bat',
+	'Version',
+	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/path.py',
+	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/__init__.py',
+	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/__init__.py',
+	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/progress_bar.py',
+	'pypy2-v5.9.0-win32/lib-python/2.7/creatorUtils/compat/types.py',
 ];
 
 url = '/TheElementalOfCreation/search.py/master/{}';
-md5sCurrent = {};
-md5sNew = {};
 data = {};
+version = None;
 
 try:
-	file1 = open('md5Hashes/md5.txt');
-	for line in file1:
-		md5sCurrent[line[:-1].split('\t')[0]] = line[:-1].split('\t')[1];
-	file1.close();
+	
 except:
-	for x in files:
-		file1 = open(x, 'rb');
-		md5sCurrent[x] = _md5.new(file1.read()).hexdigest();
-		file1.close();
+	
 
 a = httplib.HTTPSConnection('raw.githubusercontent.com');
 for x in files:
 	a.request('GET', url.format(x));
 	data[x] = a.getresponse().read();
-	md5sNew[x] = _md5.new(data[x]).hexdigest();
-	if md5sNew[x] != md5sCurrent[x]:
-		file1 = open(x, 'wb');
-		file1.write(data[x]);
-		file1.close();
-
-
-file2 = open('md5Hashes/md5.txt', 'w');
-for x in files:
-	file2.write(x + '\t' + md5sNew[x] + '\n');
-file2.close();
+	
